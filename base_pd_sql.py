5eb7294ad11bac9077182da86e3d044416ca6aec
ryabovd <ryabovd@outlook.com>
2022-08-01 19:28:41 +0700

    Add executemany_query func. Test base func (not for release).

--- base_pd_sql.py
+++ base_pd_sql.py
@@ -27,26 +27,34 @@ numbers = white_text_on_blue
 def main():
     print("SQL")
     connection = create_connection("persons.sqlite")
-
     create_persons_table = """
         CREATE TABLE IF NOT EXISTS persons (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
-        date_of_birth DATE,
+        date_of_birth DATA,
         place_of_birth TEXT (50),
         passport TEXT (80),
         snils TEXT (11),
         inn TEXT (12),
         address TEXT (80),
         phone TEXT (12),
-        email TEXT (256)
+        email TEXT (256),
+        actual_date DATA
         );
         """
 
     execute_query(connection, create_persons_table)
+    #test_list_of_data()
 
-    
-    
+
+
+    list_of_data = get_list_of_data()
+    #print(list_of_data)
+    add_many_records = """INSERT INTO persons VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+    executemany_query(connection, add_many_records, list_of_data)
+
+    pass
+        
     base_structure = [
         'Фамилия Имя Отчество (с учетом РЕГИСТРА)', 
         'Дата рождения dd.mm.yyyy', 
@@ -61,14 +69,29 @@ def main():
         ]
 
 
+def test_list_of_data():
+    list_of_data = get_list_of_data()
+    for rec in list_of_data:
+        if len(rec) < 11:
+            print(rec)
+    base_file = "base_pd.csv"
+    base = base_file_read(base_file)
+    i = 1
+    for rec in base:
+        rec = [i] + rec
+        list_of_data.append(tuple(rec))
+        i += 1
+        #print(( ';').join(rec) + '\n')
+    return list_of_data
     pass
-    
+
 
 def create_connection(path):
     connection = None
     try:
         connection = sqlite3.connect(path)
         print("Connection to SQLite DB successful")
+        # Add green text
     except Error as e:
         print(f"The error '{e}' occurred")
     return connection
@@ -80,8 +103,40 @@ def execute_query(connection, query):
         cursor.execute(query)
         connection.commit()
         print("Query executed successfully")
+        # Add green text
     except Error as e:
         print(f"The error '{e}' occurred")
+
+def get_list_of_data():
+    list_of_data = []
+    base_file = "base_pd.csv"
+    base = base_file_read(base_file)
+    i = 1
+    for rec in base:
+        rec = [i] + rec
+        list_of_data.append(tuple(rec))
+        i += 1
+        #print(( ';').join(rec) + '\n')
+    return list_of_data
+    pass
+
+def base_file_read(base_file):
+    '''Func that reads csv file (delimiter is ';') and returns a list of lists of strings'''
+    with open(file=base_file, mode='r', encoding='utf-8') as base:
+        lines = csv.reader(base, delimiter=';')
+        base_list = list(lines)
+    return base_list
+
+def executemany_query(connection, query, list_of_data):
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(query, list_of_data)
+        connection.commit()
+        print("Query executed successfully")
+        # Add green text
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    pass
 
 
 if __name__ == "__main__":
