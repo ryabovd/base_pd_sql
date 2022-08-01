14d2715cfff85fb309e2876e5db03e840381201b
ryabovd <ryabovd@outlook.com>
2022-08-01 17:38:18 +0700

    Add func execute_query. Add table. Create db.

--- base_pd_sql.py
+++ base_pd_sql.py
@@ -26,7 +26,41 @@ numbers = white_text_on_blue
 
 def main():
     print("SQL")
-    connection = create_connection("person.sqlite")
+    connection = create_connection("persons.sqlite")
+
+    create_persons_table = """
+        CREATE TABLE IF NOT EXISTS persons (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        date_of_birth DATE,
+        place_of_birth TEXT (50),
+        passport TEXT (80),
+        snils TEXT (11),
+        inn TEXT (12),
+        address TEXT (80),
+        phone TEXT (12),
+        email TEXT (256)
+        );
+        """
+
+    execute_query(connection, create_persons_table)
+
+    
+    
+    base_structure = [
+        'Фамилия Имя Отчество (с учетом РЕГИСТРА)', 
+        'Дата рождения dd.mm.yyyy', 
+        'Место рождения', 
+        'Паспорт (Номер, Кем выдан, Дата выдачи, Код подразделения)', 
+        'СНИЛС (только цифры)', 
+        'ИНН', 
+        'Адрес (Индекс, Регион, Город, Улица, Дом, Квартира)', 
+        'Телефон (+7********** несколько через запятую)', 
+        'e-mail (несколько через запятую)', 
+        'Дата актуальности'
+        ]
+
+
     pass
     
 
@@ -38,6 +72,16 @@ def create_connection(path):
     except Error as e:
         print(f"The error '{e}' occurred")
     return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
 
 if __name__ == "__main__":
