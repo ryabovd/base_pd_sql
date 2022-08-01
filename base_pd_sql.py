30afaea4aeba604325ea34e8ec013232b769833d
ryabovd <ryabovd@outlook.com>
2022-08-01 12:13:49 +0700

    Add color text. Add main func. Add create_connection func.

---
+++ base_pd_sql.py
@@ -0,0 +1,43 @@
+import sqlite3
+from sqlite3 import Error
+import csv
+from datetime import date
+import sys
+import ctypes
+import pprint
+
+
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+'''
+Colors!
+Write a module and import in future.
+'''
+red_text = '\033[31m'
+green_text = '\033[32m'
+yellow_text = '\033[33m'
+blue_text = '\033[34m'
+white_text_on_blue = '\033[37m\033[44m'
+marked_text = '\033[43m'
+end_text = '\033[0m'
+numbers = white_text_on_blue
+
+
+def main():
+    print("SQL")
+    
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+if __name__ == "__main__":
+    main()
+    print(green_text + 'Работа программы ЗАВЕРШЕНА' + end_text)
